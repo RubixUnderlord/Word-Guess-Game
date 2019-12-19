223cc246f126bb3cbf919adec56cd408708ab395
RubixUnderlord <57545547+RubixUnderlord@users.noreply.github.com>
2019-12-18 21:35:58 -0700

    built in ascii keys

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,58 +1,78 @@
 $(document).ready(function () {
+    var wins = 0
+    var loses = 0
+    var life = 13;
     var wordBank = ["ABRUBPTLY", "FUSHCIA", "HAPHAZARD", "QUARTZ", "LYMPH"];
-    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var wordLength = [];
-
-    function restart(){
-        var restartBtn = $("<button>");
+    $("#start").on("click", function () {
+        start()
+    });
+    
+    var restartBtn = $("<button>");
         restartBtn.addClass("col");
         restartBtn.text("Restart");
-        restartBtn.attr('id', 'start');
+        restartBtn.attr('id', 'restart remove');
         $("#restart").append(restartBtn);
-        $("#hide").removeAttr('id', 'hide');
-    }
+        $("#restart").on("click", function () {
+            $(this).attr('id', 'hide');
+            start()
+        });
 
-    $("#start").click(function () {
-        var life = 13;
+
+    function start() {
+        life = 13
+        console.log("something");
         var randomIndex = Math.floor(Math.random() * wordBank.length);
         var chosenWord = wordBank[randomIndex];
         $("#start").addClass("hide");
         $(".game").removeClass("hide");
+        $("#wins").text(`wins: ${wins}`);
+        $("#loses").text(`loses: ${loses}`);
+        var wordLength = [];
         for (var i = 0; i < chosenWord.length; i++) {
             wordLength[i] = "_";
         }
 
 
-        for (var i = 0; i < letters.length; i++) {
+        for (var i = 65; i < 91; i++) {
             var letterBtn = $("<button>");
             letterBtn.addClass("col-md-1 visible");
-            letterBtn.attr("data-letter", letters[i]);
-            letterBtn.text(`${letters[i]}`);
+            letterBtn.attr("data-letter", String.fromCharCode(i));
+            letterBtn.text(String.fromCharCode(i));
             letterBtn.click(function () {
                 var chosenLetter = $(this).attr("data-letter");
                 $(this).addClass("hide");
                 $(this).off();
                 console.log(chosenLetter);
                 console.log(life);
-                var wordPreGuess = wordLength;
-                for (var l = 0; l < chosenWord.length; l++) {
+                for (var l = 0; l < chosenWord.length + 1; l++) {
                     if (chosenWord[l] === chosenLetter) {
-                        wordLength[l] = chosenLetter;
-                        $("#remainingWord").text(`Word: ${wordLength.join(" ")}`);
-
+                        for (var p = l; p < chosenWord.length; p++) {
+                            if (chosenWord[p] === chosenLetter) {
+                                wordLength[p] = chosenLetter;
+                                $("#remainingWord").text(`Word: ${wordLength.join(" ")}`);
+                            }
+                        }
+                        break
                     }
-                }
-                if (wordPreGuess == wordLength) {
-                    life--
+                    if (l === chosenWord.length) {
+                        life--
+                        break
+                    }
                 }
                 if (life === parseInt(0)) {
                     $("#remainingWord").text("You Lose");
                     $(".visible").addClass("disappear");
-                    restart();
+                    loses++
+                    console.log(`${loses}`);
+                    $("#hide").removeAttr('id', 'hide');
+                    $("#loses").text(`loses: ${loses}`);
                 } else if (chosenWord === wordLength.join("")) {
                     $("#remainingWord").text(`You Win`);
                     $(".visible").addClass("disappear");
-                    restart();
+                    wins++
+                    console.log(`${wins}`);
+                    $("#hide").removeAttr('id', 'hide');
+                    $("#wins").text(`wins: ${wins}`);
                 } else {
                 }
 
@@ -65,5 +85,5 @@ $(document).ready(function () {
 
 
         $("#remainingWord").text(`Word: ${wordLength.join(" ")}`);
-    });
+    };
 });
